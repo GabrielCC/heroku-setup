4a5aa5d8a73e10c2f99d6fcc30621559d91027d2
gabrielcc <gabriel.croitoru.cristian@gmail.com>
2011-11-09 20:17:33 +0000

    changed the server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World from the c9 editor!');
 });
 
 var port = process.env.PORT || 3000;
